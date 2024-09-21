d197a88cdb8cd4f7aad0c084fb908fe55e2d1387
gxjh2 <gxjh205@163.com>
2024-09-21 20:48:42 +0800

    返回对象公共方法

--- src/main/java/com/example/lv/bean/base/ResultGenerator.java
+++ src/main/java/com/example/lv/bean/base/ResultGenerator.java
@@ -44,7 +44,7 @@ public class ResultGenerator {
     /**
      * 成功返回数据
      */
-    public static Result getSuccessResult(Map<String,Object> bean) {
+    public static Result getSuccessBeanResult(Map<String,Object> bean) {
         return new Result()
                 .setCode(ResultCode.SUCCESS)
                 .setMessage(DEFAULT_SUCCESS_MESSAGE)
